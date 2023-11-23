df81d9f6031c244a48394a82789d961d2feb8782
phyusin <rollyphyusinkhant7290@gmail.com>
2023-11-23 16:19:46 +0630

    finished login form ui

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,53 +1,84 @@
-// 'use client';
-// import React from 'react'
-// import { SubmitHandler, useForm } from "react-hook-form";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import * as z from "zod";
-// import { Input } from "../ui/input";
-// import { Button } from "../ui/button";
-// import FormErrorText from "../FormErrorText";
-// import Link from "next/link";
-// import axios from "axios";
-// import { SignupData } from "@/types/auth";
-// import { useRouter } from "next/navigation";
-// import toast, { Toaster } from "react-hot-toast";
+"use client";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import FormErrorText from "../FormErrorText";
+import Link from "next/link";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
 
-// const LoginForm = () => {
-//   return (
-//     <div className=" flex justify-center items-center h-screen ">
-//       <Toaster />
+const LoginForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
-//       <form
-//         action=""
-//         onSubmit={handleSubmit(onSubmit)}
-//         className="w-full md:w-[25%] p-4 flex flex-col gap-y-4"
-//       >
-//         <h1 className=" text-lg font-bold  text-center">Signup</h1>
+  const LoginSchema = z.object({
+    email: z.string().min(1, "Email is required").email(),
+    password: z.string().min(6).max(20),
+  });
 
-//         <Input type="text" placeholder="Name" {...register("name")} />
-//         <FormErrorText errors={errors} fieldname="name" />
+  type LoginSchemaType = z.infer<typeof LoginSchema>;
 
-//         <Input type="email" placeholder="Email" {...register("email")} />
-//         <FormErrorText errors={errors} fieldname="email" />
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchemaType>({ resolver: zodResolver(LoginSchema) });
 
-//         <Input type="text" placeholder="Password" {...register("password")} />
-//         <FormErrorText errors={errors} fieldname="password" />
+  const onSubmit: SubmitHandler<LoginSchemaType> = async (data) => {
+    try {
+      setIsLoading(true);
+      await axios.post("/api/login", data);
+      router.push("/");
+    } catch (error: any) {
+      setIsLoading(false);
 
-//         <div className="flex justify-between items-center">
-//           <Button
-//             disabled={isLoading ? true : false}
-//             variant="outline"
-//             className={`flex-1 ${isLoading && "opacity-10"}`}
-//           >
-//             Signup
-//           </Button>
-//           <Link href="/login" className="text-sm p-5">
-//             Already have an account?
-//           </Link>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// }
+      return toast.error(
+        `${
+          error.response.status === 500
+            ? "Something went wrong!"
+            : error.response.data.message
+        }`
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className=" flex justify-center items-center h-screen ">
+      <Toaster />
 
-// export default LoginForm
+      <form
+        action=""
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full md:w-[25%] p-4 flex flex-col gap-y-4"
+      >
+        <h1 className=" text-lg font-bold  text-center">Login</h1>
+
+        <Input type="email" placeholder="Email" {...register("email")} />
+        <FormErrorText errors={errors} fieldname="email" />
+
+        <Input type="text" placeholder="Password" {...register("password")} />
+        <FormErrorText errors={errors} fieldname="password" />
+
+        <div className="flex justify-between items-center">
+          <Button
+            disabled={isLoading ? true : false}
+            variant="outline"
+            className={`flex-1 ${isLoading && "opacity-10"}`}
+          >
+            {isLoading ? "Processing..." : "Login"}
+          </Button>
+          <Link href="/signup" className="text-sm p-5">
+            Do not have an account?
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
