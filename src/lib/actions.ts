b45595d51611b41d7c6dc21ff739b6ab5996c03b
phyusin <rollyphyusinkhant7290@gmail.com>
2023-11-29 16:20:19 +0630

    wip signup

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,6 +1,9 @@
 "use server";
+import bcrypt from "bcrypt";
 import { auth, signIn, signOut } from "@/auth";
 import { redirect } from "next/navigation";
+import { connectToDB } from "./mongodb";
+import UserModel from "@/models/User";
 
 export async function authenticate(
   prevState: string | undefined,
@@ -26,4 +29,24 @@ export async function getUserCredentials() {
   const result = await auth();
   if (!result) return redirect("/login");
   return result;
+}
+
+export async function signup(formData: FormData) {
+  try {
+    console.log("here", Object.fromEntries(formData));
+    connectToDB();
+
+    const password = formData.get("password");
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = bcrypt.hash(password, salt);
+
+    await UserModel.create({
+      ...Object.fromEntries(formData),
+      password: hashedPassword,
+    });
+  } catch (error) {
+    console.log(error);
+    return "Failed to signup user";
+  }
+  redirect("/");
 }
