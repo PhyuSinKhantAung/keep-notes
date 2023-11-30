9d5b121c12dcec2bb1fbc37b01578f531c5db87e
phyusin <rollyphyusinkhant7290@gmail.com>
2023-11-30 16:08:25 +0630

    added also pinned and others ui

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -15,7 +15,25 @@ const page = () => {
     <div>
       <NoteInputsForm />
 
+      <div className="grid grid-cols-12 gap-y-4 gap-x-4 p-16">
+        <small className="col-span-12">Pinned</small>
+
+        <Card className="lg:col-span-3 md:col-span-6 col-span-12 bg-background">
+          <CardHeader className="p-4">
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+
+          <CardFooter className="flex gap-x-2 p-4 cursor-pointer justify-between">
+            <Copy size={18} />
+            <ArchiveRestore size={18} />
+            <Trash2 size={18} />
+            <Pin size={18} />
+          </CardFooter>
+        </Card>
+      </div>
+
       <div className="grid grid-cols-12  gap-y-4 gap-x-4 p-16">
+        <small className="col-span-12">Others</small>
         <Card className="lg:col-span-3 md:col-span-6 col-span-12 bg-background">
           <CardHeader className="p-4">
             <CardDescription>Card Description</CardDescription>
